bea4ac49bd28c8b55b07f9ee03e9e4426dc983a3
raspu <raspu@raspu.com>
2012-06-06 11:28:26 -0400

    Initial Commit

---
+++ RadarChart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RadarChart
+//
+//  Created by Juan Pablo Illanes Sotta on 06-06-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RadarChart/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  RadarChart
+//
+//  Created by Juan Pablo Illanes Sotta on 06-06-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
